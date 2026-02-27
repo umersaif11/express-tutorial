1d0745af48ca898ba2ac172956ab98decafd9e3e
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-27 16:50:32 +0500

    Add html page in the server routing

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http')
+const {readFileSync} = require('fs')
+
+const homepage = readFileSync('./index.html')
 
 const server = http.createServer((req, res) => {
     const url = req.url
     if(url === '/') {
         res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>home page</h1>')
+        res.write(homepage)
         res.end()
     } else if(url === '/about') {
         res.writeHead(200, {'content-type': 'text/html'})
