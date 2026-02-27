034128791913164f91e4c1e4d2ca5f655b2f3294
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-27 19:08:18 +0500

    Read other resources and set their http paths along with header and read files

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http')
 const {readFileSync} = require('fs')
 
+//get all files
 const homepage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req, res) => {
     const url = req.url
@@ -10,9 +14,17 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'content-type': 'text/html'})
         res.write(homepage)
         res.end()
-    } else if(url === '/about') {
-        res.writeHead(200, {'content-type': 'text/html'})
-        res.write('<h1>about page</h1>')
+    } else if(url === '/styles.css') {
+        res.writeHead(200, {'content-type': 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    } else if(url === '/browser-app.js') {
+        res.writeHead(200, {'content-type': 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    } else if(url === '/logo.svg') {
+        res.writeHead(200, {'content-type': 'image/svg+xml'})
+        res.write(homeImage)
         res.end()
     } else {
         res.writeHead(404, {'content-type': 'text/html'})
