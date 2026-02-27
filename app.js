72956615d82b2ad9edadbfc40037bef07dc62f8c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-27 15:19:22 +0500

    Create a http server and return header along with content in the response object

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Express tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/html'})
+    res.end('<h1>home page</h1>')
+})
+server.listen(5000)
