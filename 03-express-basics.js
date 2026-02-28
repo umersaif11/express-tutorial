b384d67169abdc4989ba6538f88234631e27f80b
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-28 15:25:28 +0500

    Give routing for home, about and for any other case

--- 03-express-basics.js
+++ 03-express-basics.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    console.log('User hit the home page')
+    res.send('Home page')
+})
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+app.all('*', (req, res) => {
+    res
+    .status(404)
+    .send('<h1>Resource not found</h1>')
+})
+
 app.listen(5000, () => {
     console.log('Server is listening to port 5000')
 })
