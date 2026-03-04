fc682cf8ec97ed09bf35d5b5b42969c028de4ddb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-04 22:00:25 +0500

    middleware basic file

--- 08-middleware-basics.js
+++ 08-middleware-basics.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
+
+const logger = (req, res, next) => {
+    const method = req.method
+    const url = req.url
+    const time = new Date().getFullYear()
+    console.log(method, url, time)
+    next()
+}
 
 app.get('/',logger, (req, res) => {
     res.send('Home')
