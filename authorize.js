4ef36f46734f97dbc97c2bc5102dbd243aa07629
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-04 22:53:40 +0500

    Authrize middleware is working as a filter or as a bouncer in front of a club

--- authorize.js
+++ authorize.js
@@ -1,4 +1,10 @@
 const authorize = (req, res, next) => {
-    console.log('authorize')
+    const {user} = req.query
+    if(user === 'john') {
+        req.user = {name: 'john', id: 3}
+        next()
+    } else {
+        res.status(401).send('Unauthorized')
+    }
 }
 module.exports = authorize
