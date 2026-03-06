a282e4c7ec526058ff16243a3eb3a8f8c519da7e
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-06 15:53:35 +0500

    'morgan' is 3rd party middleware npm package

--- 10-middleware-options.js
+++ 10-middleware-options.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const logger = require('./logger')
 const authorize = require('./authorize')
 
 // app.use([logger, authorize])
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
     res.send('Home')
@@ -14,7 +16,7 @@ app.get('/about', (req, res) => {
 app.get('/api/products', (req, res) => {
     res.send('Products')
 })
-app.get('/api/items', [logger, authorize], (req, res) => {
+app.get('/api/items', (req, res) => {
     res.send('Items')
 })
 
