bab72b176cee367fbb60a3909d75923d0ba1a1cb
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-06 19:07:16 +0500

    POST Method is if you send something to server

--- 11-methods.js
+++ 11-methods.js
@@ -7,6 +7,9 @@ app.use(express.static('./methods-public'))
 app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people})
 })
+app.post('/login', (req, res) => {
+    res.send('Welcome')
+})
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000...')
