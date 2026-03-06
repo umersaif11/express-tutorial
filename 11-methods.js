3ee03c4170fdc5e6b83ba71cd36348658b0c4289
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-03-06 19:55:29 +0500

    If form send empty value then add request to give name

--- 11-methods.js
+++ 11-methods.js
@@ -12,8 +12,11 @@ app.get('/api/people', (req, res) => {
     res.status(200).json({success: true, data: people})
 })
 app.post('/login', (req, res) => {
-    console.log(req.body)
-    res.send('Welcome')
+    const {name} = req.body
+    if(name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide credentials')
 })
 
 app.listen(5000, () => {
